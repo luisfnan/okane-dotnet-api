50b70475af99e5e20810e46854ba676940459692
luisfnan <fernando-9813@hotmail.com>
2023-08-24 19:35:30 -0400

    optional field called InvoiceUrl

---
+++ Okane.Storage.EntityFramework/Migrations/20230824233316_firstM.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Okane.Storage.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Category = table.Column<string>(type: "longtext", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false),
+                    InvoiceUrl = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+        }
+    }
+}
---
+++ Okane.Storage.EntityFramework/OkaneDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Okane.Core.Entities;
+
+
+namespace Okane.Storage.EntityFramework;
+
+public class OkaneDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; } = null!;
+
+    public OkaneDbContext()
+    {
+    }
+
+    public OkaneDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseMySQL("Host=localhost;Port=33060;Database=pokemonTest;Username=root;Password=password;");
+    }
+}
